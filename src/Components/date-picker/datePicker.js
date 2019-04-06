fe5b3d5f2d94889afdd5516f00face158ce0a768
misterhtmlcss <misterhtmlcss@gmail.com>
2019-04-06 15:55:31 -0600

    Stubbed DatePicker

---
+++ src/Components/date-picker/datePicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
+
+const datePicker = () => (
+  <h1>DatePicker</h1>
+);
+
+export default datePicker;
