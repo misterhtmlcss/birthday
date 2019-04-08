73b1122807adcbda9fb65e69876d86d58db6d993
misterhtmlcss <misterhtmlcss@gmail.com>
2019-04-07 20:14:06 -0600

    added a datepicker component

--- src/Components/date-picker/datePicker.js
+++ src/Components/date-picker/datePicker.js
@@ -3,7 +3,10 @@ import DayPicker from "react-day-picker";
 import "react-day-picker/lib/style.css";
 
 const datePicker = () => (
-  <h1>DatePicker</h1>
+  <>
+    <h1>DatePicker</h1>
+    <DayPicker selectedDays={new Date()} />
+  </>
 );
 
 export default datePicker;
