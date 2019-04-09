e21dd059d30d243cd4c8465b31762f1173266c36
Marcela Wang <marcelawang7@gmail.com>
2019-04-09 16:15:28 -0600

    Inputs components with styling(Google Fonts)

---
+++ src/Components/Inputs/Inputs.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style.css"
+
+const Inputs = (props) => {
+return(
+    <div >
+    <input placeholder={props.name}></input>
+    </div>
+)
+
+
+}
+
+export default Inputs 
