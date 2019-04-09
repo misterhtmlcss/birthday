e599aac79a1222b7777648f65624473be9c523a9
Marcela Wang <marcelawang7@gmail.com>
2019-04-09 16:12:39 -0600

    added Google Fonts to Titles

---
+++ src/Components/Titles/Titles.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style.css"
+
+
+const Title = (props) => {
+    return (
+        <div class="title">
+            {props.name}
+            <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet"></link>
+        </div>
+    )
+
+}
+export default Title
