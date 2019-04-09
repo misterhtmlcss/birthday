e9a80dd23835731abbf9a1c21c67fd3f79addf93
Marcela Wang <marcelawang7@gmail.com>
2019-04-09 17:49:15 -0600

    added logo to MainPage

--- src/Views/MainPage/MainPage.js
+++ src/Views/MainPage/MainPage.js
@@ -3,12 +3,14 @@ import Title from "../../Components/Titles/Titles"
 import Subtitle from "../../Components/Subtitles/Subtitles"
 import Inputs from "../../Components/Inputs/Inputs"
 import "./style.css"
+import { ReactComponent as Logo } from '../../Assets/logo5.svg';
 
 const lorem = "Welcome to the Birthday App! Sign up and receive ONE email on your birthday with the best deals in town! Hundreds of free gifts are waiting for you! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 
 const MainPage = () => {
     return (
         <div class="div">
+            <Logo class= "logo"/>
             <Title class="title" name="Birthday" />
             <Subtitle class="subtitle" name={lorem} /><br />
             <Inputs class="inputs" name=" Location" />
