a40cf4f2f4e105da54c32b76d7f169031f8ab96d
Marcela Wang <marcelawang7@gmail.com>
2019-04-12 00:35:31 -0600

    changed CSS on Main Page

--- src/Views/MainPage/MainPage.js
+++ src/Views/MainPage/MainPage.js
@@ -4,19 +4,21 @@ import Subtitle from "../../Components/Subtitles/Subtitles"
 import Inputs from "../../Components/Inputs/Inputs"
 import "./style.css"
 import { ReactComponent as Logo } from '../../Assets/logo5.svg'
-import Btn from "../../Components/button/btnMain"
+import MainBtn from "../../Components/Button/MainBtn/MainBtn"
 
 
 const text = "Welcome to the Birthday App! Sign up and receive ONE email on your birthday with the best deals in town! Hundreds of free gifts are waiting for you! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 
 const MainPage = (props) => {
     return (
-        <div class="div">
-            <Logo class= "logo"/>
-            <Title class="title" name="Birthday" />
-            <Subtitle class="subtitle" name={text} /><br />
-            <Inputs class="inputs" name=" Location" /><br/>
-            <Btn changePage={props.changePage} name="Let's go!"/><br/>
+        <div className="main-container">
+            <div className="logo-container">
+                <Logo class="logo" />
+                <Title class="title" name="Birthday" />
+            </div>
+            <Subtitle class="subtitle" name={text} />
+            <Inputs class="inputs" name=" Location" />
+            <MainBtn changePage={props.changePage} name="Let's go!" />
         </div>
     )
 
